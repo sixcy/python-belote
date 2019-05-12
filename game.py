6a219d472c2c6d818424af1f2707cdf2138852f1
Cyril SIX <cyril.six@univ-grenoble-alpes.fr>
2019-05-12 22:18:47 +0200

    Someone else than first_player may accept the atout

--- game.py
+++ game.py
@@ -2,12 +2,14 @@ from typing import List, Optional
 from cards import *
 from table import Table
 from player import Player
+from utils import maybe
 
 class Gamestate:
   def __init__(self, table: Table, first_player: Player) -> None:
     self.table = table
     self.first_player = first_player
     self.atout : Optional[Color] = None
+    self.preneur : Optional[Player] = None
 
   @property
   def iter_players(self) -> List[Player]:
@@ -17,7 +19,8 @@ class Gamestate:
   def __str__(self) -> str:
     return ("{ table: " + str(self.table)
             + ", \nfirst player: " + str(self.first_player.name)
-            + ", \natout: " + str(self.atout) + "}")
+            + ", \natout: " + str(self.atout)
+            + ", \npreneur: " + (str(self.preneur.name) if self.preneur else "None") + "}")
 
 
 def init_deck() -> Deck:
@@ -27,13 +30,27 @@ def init_deck() -> Deck:
       L.append(Card(color, rank))
   return Deck(L)
 
+
 ## PLAN
+#
+# Common ground: the deck is initialized and randomized
+# 
+# Situation A:
+#   1) Each player draws 5 cards from the deck.
+#
+# Situation B:
+#   1) A player is designed as "first player", and players draw 3+2 cards from the deck
+#       in that order
+#
+# Situation C:
+#   1) Situation B
+#   2) The top card of the deck is revealed and put faceup
 #
 # Situation D:
 #   1) Situation C
 #   2) The first player accepts the card, and gets 2 cards - then the others get 3 cards
 #
-# Situation E:
+# --> Situation E: <--
 #   1) Situation C
 #   2) The first player may accept the card, same for the rest.
 #       It goes clockwise until someone accepts
@@ -63,11 +80,20 @@ def game_loop() -> None:
   # Revealing top card of the deck
   table.deck.reveal()
 
-  # First player takes it
+  # Loop until someone takes the first card
   assert table.deck.topcard is not None
-  state.atout = table.deck.topcard.color
+  while not state.preneur:
+    for player in state.iter_players:
+      if maybe():
+        state.atout = table.deck.topcard.color
+        state.preneur = player
+        player.draw(deck)
+        break
+    
+  # Final distribution
   for player in state.iter_players:
-    for j in range(3):
+    n_cards = 2 if player is state.preneur else 3
+    for j in range(n_cards):
       player.draw(deck)
 
   print("Final state: ", state)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from typing import *
+import random
 
 def list_str(L: List) -> str:
   return "[" + ", ".join(str(elt) for elt in L) + "]"
@@ -6,3 +7,6 @@ def list_str(L: List) -> str:
 def list_rotate(L: List, n: int) -> List:
   assert n in range(len(L))
   return L[n:] + L[:n]
+
+def maybe() -> bool:
+  return random.choice([True, False])
